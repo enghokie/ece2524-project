c70a102914ad5749f5a4653ad26b339736aa37fe
Siraj Ali <alisiraj@vt.edu>
2014-05-08 12:22:13 -0400

    c99 brah

--- game.c
+++ game.c
@@ -164,7 +164,8 @@ int wordConverter(char* word)
 {
   int array[21] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
   char* buf[21] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen","eighteen", "nineteen", "twenty"};
-  for (int i = 0; i < 21; i++)
+  int i;
+  for (i = 0; i < 21; i++)
   {
     if (!strncmp(word, buf[i], 100))
 	return array[i];
